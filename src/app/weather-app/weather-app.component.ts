f70847012075b3504edcc3478415fb4558f6f7fe
seanrandevs <93683781+seanrandevs@users.noreply.github.com>
2024-07-29 15:11:50 -0600

    Made Api call and sent it to weather  component.

--- src/app/weather-app/weather-app.component.ts
+++ src/app/weather-app/weather-app.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-weather-app',
   standalone: true,
-  imports: [],
+  imports: [HttpClientModule],
   templateUrl: './weather-app.component.html',
   styleUrl: './weather-app.component.css'
 })
-export class WeatherAppComponent {
 
+export class WeatherAppComponent implements OnInit  {
+  public data:any;
+  
+  constructor(private http: HttpClient) {
+
+  }
+
+  ngOnInit(): void {
+    this.fetchDetails();
+  }
+
+  public fetchDetails() {
+    this.http.get(`https://api.openweathermap.org/data/2.5/weather?q=
+    dallas&units=imperial&appid=c412fb8f1ed42194a962dd8b85f34c0c`).subscribe(
+      (resp:any) => {
+        console.log(resp);
+        this.data = resp;
+      }
+    )
+  }
 }
+
+
+// const api = `https://api.openweathermap.org/data/2.5/weather?q=
+//     ${location}&units=imperial&appid=c412fb8f1ed42194a962dd8b85f34c0c`;
