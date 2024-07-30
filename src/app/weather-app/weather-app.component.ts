8e2b8f35cc85813765df0a5caadf3fb1a0dbaa21
seanrandevs <93683781+seanrandevs@users.noreply.github.com>
2024-07-30 15:12:22 -0600

    Finished the Javascript. Just have to add some errors

--- src/app/weather-app/weather-app.component.ts
+++ src/app/weather-app/weather-app.component.ts
@@ -1,25 +1,24 @@
+import { NgIf } from '@angular/common';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-weather-app',
   standalone: true,
-  imports: [HttpClientModule],
+  imports: [HttpClientModule, NgIf],
   templateUrl: './weather-app.component.html',
   styleUrl: './weather-app.component.css'
 })
 
-export class WeatherAppComponent implements OnInit  {
-  public data:any;
+export class WeatherAppComponent {
+  data:any;
   location: string = '';
   
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient) {}
 
-  }
-
-  ngOnInit(): void {
-    this.fetchDetails();
-  }
+   currDate = new Date().toLocaleDateString();
+   currTime = new Date().toLocaleTimeString(
+    'en-US', {hour: '2-digit', minute: '2-digit'});
 
   onLocationChange(event: Event): void {
     const inputElement = event.target as HTMLInputElement;
@@ -30,18 +29,23 @@ export class WeatherAppComponent implements OnInit  {
 
   public fetchDetails(event?: KeyboardEvent): void {
     if (!event ||event.key === 'Enter') {
+      console.log(`Fetching weather data for location: ${location}`);
+      if (!location) {
+        console.error('Location is empty');
+        return;
+      }
       this.http.get(`https://api.openweathermap.org/data/2.5/weather?q=
         ${this.location}&units=imperial&appid=c412fb8f1ed42194a962dd8b85f34c0c`).subscribe(
           (resp:any) => {
             console.log(resp);
             this.data = resp;
-          })
+          },
+          error => {
+            console.error('API call failed', error);
+          }
+        );
           this.location = '';
     }
 
   }
 }
-
-
-// const api = `https://api.openweathermap.org/data/2.5/weather?q=
-//     ${location}&units=imperial&appid=c412fb8f1ed42194a962dd8b85f34c0c`;
